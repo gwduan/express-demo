008932f57c04f1c1442e32d65beb0f4585ce6597
Dennis Duan <gwduan@gmail.com>
2014-08-27 14:49:17 +0800

    Implement API: /users/regsiter.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,89 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/mymongo');
+var cache = require('../models/myredis');
+var errlog = require('../errlog');
+var debug = require('debug')('express-demo');
+var valid = require('validator');
+var format = require('date-format');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
+
+router.post('/register', function(req, res) {
+    registerProc(db, req, res);
+});
+
+function registerProc(db, req, res) {
+  var user_id = req.body.phone;
+  var user_name = req.body.name;
+  var user_pass = req.body.password;
+
+  debug('user_id:' + user_id);
+  debug('user_name:' + user_name);
+  debug('user_pass:' + user_pass);
+
+  if (!valid.isNumeric(user_id) || !valid.isLength(user_id, 11, 11)) {
+    res.send({code: -1, info: '输入数据错误!'});
+    return;
+  }
+  if (valid.isNull(user_name)) {
+    res.send({code: -1, info: '输入数据错误!'});
+    return;
+  }
+  if (!valid.isLength(user_pass, 6)) {
+    res.send({code: -1, info: '输入数据错误!'});
+    return;
+  }
+
+  var user_doc = {
+    '_id': user_id,
+    'name': user_name,
+    'password': user_pass,
+    'reg_date': new Date()
+  };
+
+  db.collection('users', function(err, collection) {
+    if (err) {
+      errlog('Open collection ' + err);
+      res.send({code: -1, info: '数据库操作失败!'});
+      return;
+    }
+
+    collection.insert(user_doc, {w: 1}, function(err, records) {
+      if (err) {
+        errlog('Insert documents ' + err);
+        if (err.message.indexOf('E11000') != -1) {
+          res.send({code: -1, info: '用户已存在，请勿重复注册!'});
+        } else {
+          res.send({code: -1, info: '数据库操作失败!'});
+        }
+        return;
+      }
+
+      res.send({code: 0, info: 'Success'});
+      incUserTotal(user_doc.reg_date);
+    });
+  });
+};
+
+function incUserTotal(reg_date) {
+  var y_key = format.asString('yyyy', reg_date);
+  var m_key = format.asString('yyyyMM', reg_date);
+  var d_key = format.asString('yyyyMMdd', reg_date);
+
+  var multi = cache.multi();
+  multi.HINCRBY('sys.number.sum', 'users', 1);
+  multi.HINCRBY('sys.number.' + y_key, 'new_users', 1);
+  multi.HINCRBY('sys.number.' + m_key, 'new_users', 1);
+  multi.HINCRBY('sys.number.' + d_key, 'new_users', 1);
+  multi.exec(function(err, results) {
+    if (err) {
+      errlog('MULTI HINCRBY for new user registeration ' + err);
+    }
+  });
+};
 
 module.exports = router;
